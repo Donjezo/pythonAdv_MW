fa624544f0ccd840a747261fb85e458503e601cd
Donjetë Zogaj <70452463+Donjezo@users.noreply.github.com>
2026-04-27 19:18:52 +0200

    lesson 1

---
+++ lesson 1/listat.py
@@ -0,0 +1,35 @@
+
+
+emart = ["Geti","Resa","Aniki","Adonisi","Adonisi"]
+
+#print(emart[0])
+
+#print(emart)
+
+#print(emart[3])
+
+
+emart.append("hello there")
+
+#print(emart)
+
+#emart.clear()
+
+#print(emart)
+
+
+#print(emart.count("Adonisi"))
+
+print(emart)
+
+emart.reverse()
+
+print(emart)
+
+emart.insert(1,"Donjeta")
+print(emart)
+
+print(emart.index("Donjeta"))
+
+emart.remove("Geti")
+print(emart)
---
+++ lesson 1/main.py
@@ -0,0 +1,28 @@
+name = "donjeta"
+print(name)
+
+name="Resa"
+print(name)
+
+
+x = 10
+y=3
+print(x-y)
+
+
+numer ="10"
+konvertimi = int(numer)
+
+
+
+print(konvertimi)
+
+print(type(x))
+
+print(type(name))
+
+numriFloat= 10.5
+
+print(numriFloat)
+
+print(type(numriFloat))
